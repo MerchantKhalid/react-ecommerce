51b9e4d3dcbd65339085274c436b9a079af768aa
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-06-24 22:55:03 +0200

    Home page layout created

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from '../features/product-list/ProductListSlice.js';
 
 export const store = configureStore({
   reducer: {
